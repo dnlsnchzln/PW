c38d095fb02eca8ccd2852a32e36589ea0161061
= <=>
2022-02-18 23:55:42 +0100

    Hecho el ejercicio 9.

--- s1/e9.php
+++ s1/e9.php
@@ -6,13 +6,37 @@
     <body>
         <?php
 
-        $array_pares = array();
+        define("TAM", 4);
 
-        for ($i = 1; $i <= 10; $i++)
-        {
-            $array_pares[$i-1] = 2 * $i;
-            print $array_pares[$i-1] . "</br>";
-        }
+        $dir = "fotos/";
+        
+        if (is_dir($dir)):
+
+            print "<table border = 3>";
+
+            $files = scandir($dir);
+            $n_files = count($files);
+
+            $k = 0;
+            for ($i = 2; $i < $n_files; $i++):
+
+                if ($k % TAM == 0):
+
+                    if ($i > 2):
+
+                        print "</tr></br>";
+                    endif;
+
+                    print "<tr>";
+                endif;
+                
+                print "<td>" . "<img src = 'fotos/" . $files[$i] . "'>" . "</td>";
+
+                $k++;
+            endfor;
+
+            print "</table>";
+        endif;
             
         ?>
     </body>
